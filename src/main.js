9ac01971280ea96f9f6731e135947a87b36e8520
yuqishuo <2095876584@qq.com>
2019-03-10 14:48:16 +0800

    【完善】【补充】协议标题的问题，补充了隐私协议

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import UserProtocol from './components/UserProtocol'
 import authorize from './components/authorize'
+import privacy from './components/privacy'
 import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false;
@@ -11,18 +12,41 @@ const routes = [
   {
     path: '/user-protocol',
     component: UserProtocol,
-    name: UserProtocol
+    name: UserProtocol,
+    meta: {
+      title: 'CrazyE用户协议'
+    }
+
   },
   {
     path: '/authorize',
     component: authorize,
-    name: authorize
+    name: authorize,
+    meta: {
+      title: '教务信息查询服务协议'
+    }
+  },
+  {
+    path: '/privacy',
+    component: privacy,
+    name: privacy,
+    meta: {
+      title: '隐私政策'
+    }
   }
 ];
 
 const router = new VueRouter({
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
 
 new Vue({
   render: h => h(App),
